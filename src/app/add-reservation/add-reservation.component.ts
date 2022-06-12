593a4f1511b3821d89362645f563f7565c74b0b2
fcport <federico.casadei.portfolio@gmail.com>
2022-06-12 22:51:45 +0200

    worked on reservation component

--- src/app/add-reservation/add-reservation.component.ts
+++ src/app/add-reservation/add-reservation.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder, FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-reservation',
   templateUrl: './add-reservation.component.html',
-  styleUrls: ['./add-reservation.component.css']
+  styleUrls: ['./add-reservation.component.css'],
 })
 export class AddReservationComponent implements OnInit {
+  addReservationForm: FormGroup;
 
-  
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.addReservationForm = formBuilder.group({
+      dateFrom: [(new Date()).toLocaleDateString(), Validators.required],
+      dateTo: [(new Date()).toLocaleDateString(), Validators.required],
+      price: ['', Validators.required],
+    });
   }
 
+  //check this out https://material.angular.io/components/datepicker/overview
+
+  ngOnInit(): void {}
+
+  addReservation() {
+    console.log(this.addReservationForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddClientComponent } from './add-client/add-client.component';
+import { AddReservationComponent } from './add-reservation/add-reservation.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ReservationsComponent } from './reservations/reservations.component';
-import { AddReservationComponent } from './add-reservation/add-reservation.component';
-import { FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import { AddClientComponent } from './add-client/add-client.component'
-import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ReservationsComponent, AddReservationComponent, AddClientComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ReservationsComponent,
+    AddReservationComponent,
+    AddClientComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,7 +30,7 @@ import {MatIconModule} from '@angular/material/icon'
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    FormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
