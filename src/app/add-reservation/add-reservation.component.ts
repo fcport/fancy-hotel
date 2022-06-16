7f26cb13e5e041e28b91038b8e8e96291b9b2419
Lilililim <federico.casadei.portfolio@gmail.com>
2022-06-16 17:50:27 +0200

    added dinamically generated clients input fields

--- src/app/add-reservation/add-reservation.component.ts
+++ src/app/add-reservation/add-reservation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {
+  Form,
+  FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -13,26 +15,37 @@ import { MatDatepicker } from '@angular/material/datepicker';
   styleUrls: ['./add-reservation.component.css'],
 })
 export class AddReservationComponent implements OnInit {
-  addReservationForm: FormGroup = new FormGroup({
-    dateFrom: new FormControl<Date | null>(new Date(), Validators.required),
-    dateTo: new FormControl<Date | null>(new Date(), ),
-    price: new FormControl<number | null>(null, Validators.required),
-  });
-
-
-  rangeFormGroup = new FormGroup({  
-    start: new FormControl(null, Validators.required),  
-    end: new FormControl(null, Validators.required)  
-  })  
+  addReservationForm: FormGroup;
 
   @ViewChild('picker') picker!: MatDatepicker<Date>;
 
   constructor(private formBuilder: FormBuilder) {
-  
+    this.addReservationForm = formBuilder.group({
+      dateFrom: new FormControl<Date | null>(new Date(), Validators.required),
+      dateTo: new FormControl<Date | null>(new Date()),
+      price: new FormControl<number | null>(null, Validators.required),
+      clients: new FormArray([]),
+    });
   }
   ngOnInit(): void {}
 
   addReservation() {
-    console.log(this.addReservationForm.value);
+    console.log(this.addReservationForm);
+  }
+  addClient() {
+    const clientInfo = this.formBuilder.group({
+      name: new FormControl<string | null>(null, [Validators.required]),
+      surname: new FormControl<string | null>(null, [Validators.required]),
+      email: new FormControl<string | null>(null, [
+        Validators.required,
+        Validators.email,
+      ]),
+      age: new FormControl<number>(0, [Validators.required]),
+    });
+    (<FormArray>this.addReservationForm.get('clients')).push(clientInfo);
+  }
+
+  getClientsControls() {
+    return (<FormArray>this.addReservationForm.get('clients')).controls;
   }
 }
