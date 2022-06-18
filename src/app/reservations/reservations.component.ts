3c06079ad39b4dc0d66bfe98b222b9db4d11264c
Lilililim <federico.casadei.portfolio@gmail.com>
2022-06-18 20:45:00 +0200

    moved mock reservations to service

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,71 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Reservation } from '../model/reservation';
-
-export const RESERVATIONS: Reservation[] = [
-  {
-    id: 'TESTTESTTEST',
-    dateFrom: new Date(),
-    dateTo: new Date(),
-    clients: [
-      {
-        name: 'Federico',
-        surname: 'Casadei',
-        age: 26,
-        email: 'fede.bbb@ccc.it',
-      },
-      {
-        name: 'Chen Jie',
-        surname: 'Ye',
-        age: 23,
-        email: 'chen.bbb@ccc.it',
-      },
-    ],
-    price: 245,
-    treatment: 'BB',
-  },
-  {
-    id: 'TESTTESTTEST',
-    dateFrom: new Date('06/16/2022'),
-    dateTo: new Date(),
-    clients: [
-      {
-        name: 'Alberto',
-        surname: 'Albertini',
-        age: 45,
-        email: 'alberto.bbb@ccc.it',
-      },
-      {
-        name: 'Cavallo',
-        surname: 'Cavallini',
-        age: 20,
-        email: 'cavallo.bbb@ccc.it',
-      },
-      {
-        name: 'Pietro',
-        surname: 'Pietrini',
-        age: 44,
-        email: 'pietro.bbb@ccc.it',
-      },
-    ],
-    price: 250,
-    treatment: 'FP',
-  },
-  {
-    id: 'TESTTESTTEST',
-    dateFrom: new Date('06/17/2022'),
-    dateTo: new Date('06/25/2022'),
-    clients: [
-      {
-        name: 'Roberto',
-        surname: 'Robewrtini',
-        age: 45,
-        email: 'robu.bbb@ccc.it',
-      },
-    ],
-    price: 33,
-    treatment: 'HP',
-  },
-];
+import { ReservationService } from './reservation.service';
 
 @Component({
   selector: 'app-reservations',
@@ -73,11 +8,12 @@ export const RESERVATIONS: Reservation[] = [
   styleUrls: ['./reservations.component.css'],
 })
 export class ReservationsComponent implements OnInit {
-  reservations: Reservation[] = RESERVATIONS;
-  displayedColumns = ['dateFrom', 'dateTo', 'price', 'treatment']
-  
+  reservations!: Reservation[];
+  displayedColumns = ['dateFrom', 'dateTo', 'price', 'treatment'];
 
-  constructor() {}
+  constructor(private reservationService: ReservationService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations();
+  }
 }
